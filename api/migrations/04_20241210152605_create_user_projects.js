c7d06d41b0eb6c8709da795cf5790779b37f7a8d
skorm11x <christopher.kosik.4@spaceforce.mil>
2024-12-10 15:40:26 +0000

    added user_projects migration and seed files

---
+++ api/migrations/04_20241210152605_create_user_projects.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('user_projects', table => {
+        table.increments('id');
+        table.integer('user_id');
+        table.integer('project_id');
+        table.dateTime('datetime_joined').notNullable();
+        table.foreign('user_id').references('user_table.id');
+        table.foreign('project_id').references('project_table.id');
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('user_projects', table => {
+        table.dropForeign('user_id')
+        table.dropForeign('project_id')
+      })
+      .then(function() {
+        return knex.schema.dropTableIfExists('user_projects')
+      });
+};
