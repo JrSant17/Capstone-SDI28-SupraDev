e05c6affb2230e0ad99e977f1077db9898963563
sixftallstar314 <brandon.d.garrett11@gmail.com>
2024-12-10 17:56:28 -0500

    added fun text to chat

--- api/seeds/03_chatposts.js
+++ api/seeds/03_chatposts.js
@@ -6,7 +6,7 @@ exports.seed = async function(knex) {
     // Deletes ALL existing entries
     await knex('chatposts').del()
     await knex('chatposts').insert([
-      {project_id: 1, user_id: 4, post_text: 'some chitter chatter pitter patter'},
+      {project_id: 1, user_id: 4, post_text: 'Team Alpha from SDI #28-30 Sept24 is Awesome'},
       {project_id: 1, user_id: 3, post_text: 'some moooore chatter'}
     ]);
   };
