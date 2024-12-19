9e2b985c2a49f6aa5c2b2f4fd1469eedeb938df4
skorm11x <christopher.kosik.4@spaceforce.mil>
2024-12-19 20:05:59 +0000

    added chat gipitee images and transition effects for routes

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './index.css';
 
 import Login from './routes/Login';
@@ -18,6 +19,7 @@ import ProjectStatus from './routes/ProjectStatus';
 
 function Content() {
   const location = useLocation();
+  const [backgroundImage, setBackgroundImage] = useState('/cyber-background.jpg');
 
   useEffect(() => {
     const routeTitles = {
@@ -37,25 +39,52 @@ function Content() {
 
     const defaultTitle = "Supra Dev";
     document.title = routeTitles[location.pathname] || defaultTitle;
+    switch (location.pathname) {
+      case '/':
+        setBackgroundImage('/cyber-background.jpg');
+        break;
+      case '/projects':
+        setBackgroundImage('/space-background-blue.png');
+        break;
+      case '/users/':
+        setBackgroundImage('/supra-profile-img.png');
+        break;
+      default:
+        setBackgroundImage('/cyber-background.jpg');
+    }
   }, [location]);
 
   return (
     <>
       {location.pathname !== "/" && <Navbar />}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin/:id" element={<AdminDashboard />} />
-        <Route path="/user/:id/projects" element={<MyProjects />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/requests" element={<ProjectSubmission />} />
-        <Route path="/projects/:id" element={<ProjectDetailsPage />} />
-        <Route path="/users" element={<ProfilePage />} />
-        <Route path="/userlist" element={<UserList />} />
-        <Route path="/myprojects" element={<MyProjects />} />
-        <Route path="/projects/:id/status" element={<ProjectStatus />} />
-      </Routes>
+      <TransitionGroup>
+        <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+          <Routes location={location}>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin/:id" element={<AdminDashboard />} />
+            <Route path="/user/:id/projects" element={<MyProjects />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/requests" element={<ProjectSubmission />} />
+            <Route path="/projects/:id" element={<ProjectDetailsPage />} />
+            <Route path="/users" element={<ProfilePage />} />
+            <Route path="/userlist" element={<UserList />} />
+            <Route path="/myprojects" element={<MyProjects />} />
+            <Route path="/projects/:id/status" element={<ProjectStatus />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+      <style>{`
+        body {
+          background-image: url(${backgroundImage});
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          transition: background-image 0.5s ease-in-out;
+        }
+      `}</style>
     </>
   );
 }
